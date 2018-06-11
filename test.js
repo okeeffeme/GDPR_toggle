46973ee7dc4d866f38e9f24ea6d5c3f9befa81b4
okeeffeme <okeeffeme@gmail.com>
2018-06-11 14:32:37 +0200

    both concats working

--- test.js
+++ test.js
@@ -10,15 +10,25 @@ let objA = {
   ]
 }
 
-function makePrivacy(a) {
+function makePrivacyA(a) {
   let objCopy = JSON.parse(JSON.stringify(a));
-  let b = 0;
+  let b = [];
   for (let i = 0; i < Object.keys(objCopy).length; i++) {
-    console.log(Object.keys(objCopy)[i].values);
+    let key = Object.keys(objCopy)[i];
+    for (let z = 0; z < objCopy[key].length; z++) {
+      b.push(objCopy[key][z]);
+    }
   }
   // console.log(b);
   console.log(b);
   return objCopy;
 }
 
-makePrivacy(objA);
+function makePrivacyB(a) {
+  let objCopy = JSON.parse(JSON.stringify(a));
+  let c = [].concat.apply([], Object.values(objCopy));
+  console.log(c);
+  return objCopy;
+}
+
+makePrivacyA(objA);

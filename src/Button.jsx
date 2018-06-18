cacf6024bb91b4bd09c196e655202c951d470141
okeeffeme <okeeffeme@gmail.com>
2018-06-18 17:30:22 +0200

    Importing react into Button

--- src/Button.jsx
+++ src/Button.jsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 <Button
   onPress={onPressLearnMore}
   title="Learn More"
--- src/Button.stories.jsx
+++ src/Button.stories.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
-import Button from 'Button';
+import Button from './Button';
 
 storiesOf('Button', module)
   .add('with text', () => (

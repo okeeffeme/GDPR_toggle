a1ea75bce3d22dd4b8dc2acf03b7757d9156900a
okeeffeme <okeeffeme@gmail.com>
2018-06-20 18:59:20 +0200

    changing onPress to onClick

--- src/Button.jsx
+++ src/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Button = (props) => (
  <button
-  onClick={props.onPress}
+  onClick={props.onClick}
   color={props.color || "#841584"}
   accessibilityLabel={props.accessibilityLabel || "Learn more about this purple button"}
   >

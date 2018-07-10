85517dac0c9ffee9c25151e71ab6746c8c7278c6
James L <somesocks@gmail.com>
2018-07-10 19:16:57 +0200

    new branch, removing old components

--- src/GDPRcontrols.stories.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
-import GDPRcontrols from './GDPRcontrols';
-
-storiesOf('GDPRcontrols', module)
-  .add('test', () => (
-    <GDPRcontrols />
-  ));
